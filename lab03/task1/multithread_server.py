122be8d45ebaa27304008728ac4816ac1b1a78e9
seva <vsevolod9006@gmail.com>
2025-03-30 14:43:04 +0300

    init

---
+++ lab03/task1/multithread_server.py
@@ -0,0 +1,55 @@
+import socket
+import threading
+import os
+
+def handle_client(client_connection):
+    request = client_connection.recv(1024).decode()
+    
+    request_lines = request.splitlines()
+    if len(request_lines) > 0:
+        request_line = request_lines[0]
+        print(f'Request: {request_line}')
+        file_name = request_line.split()[1]
+
+        if file_name.startswith('/'):
+            file_name = file_name[1:]
+
+        if os.path.exists(file_name) and os.path.isfile(file_name):
+            with open(file_name, 'rb') as f:
+                response_body = f.read()
+            response_line = 'HTTP/1.1 200 OK\n'
+        else:
+            response_body = b'<html><body><h1>404 Not Found</h1></body></html>'
+            response_line = 'HTTP/1.1 404 Not Found\n'
+
+        response_headers = 'Content-Length: {}\nContent-Type: text/html\n\n'.format(len(response_body))
+        response = response_line + response_headers
+        client_connection.sendall(response.encode() + response_body)
+    
+    client_connection.close()
+
+def start_server(server_port):
+   
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('localhost', server_port))
+    server_socket.listen(5)
+    print(f'Server started on port {server_port}...')
+
+    while True:
+        
+        client_connection, client_address = server_socket.accept()
+        print(f'Connection from {client_address}')
+
+        
+        client_thread = threading.Thread(target=handle_client, args=(client_connection,))
+        client_thread.start()
+
+if __name__ == '__main__':
+    import sys
+    
+    if len(sys.argv) != 2:
+        print("Usage: python server.py <server_port>")
+        sys.exit(1)
+    
+    port = int(sys.argv[1])
+    start_server(port)
